75b384df8399e3eeede3a20f582f22428b5f1153
Gavin <Gavin@10.80.140.104>
2018-11-20 12:13:14 -0500

    Gavin: wrote more of the encoder class

--- CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
+++ CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
@@ -1,7 +1,10 @@
 package edu.wmich.CS1120.LA6;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Encoder implements IEncoder{
@@ -24,7 +27,27 @@ public class Encoder implements IEncoder{
 			System.out.println("Error: input file not found");
 		}
 		System.out.println("Content of input file: \n" + fileRead);
-		System.out.println(count);
+		//System.out.println(count);
+		Random rand = new Random();
+		int n = 0;
+		try {
+			DataOutputStream output = new DataOutputStream(new FileOutputStream(outputFilePath));
+			for(int i = 0; i < fileRead.length(); i ++) {
+				n = rand.nextInt(20);
+				String Out = "";
+				FileOutputStream toFile = new FileOutputStream(outputFilePath, true);
+				char a = fileRead.charAt(i);
+				Out = Out + a;
+				for (int j = 0; j<=n; j++) {
+					Out = Out + n;
+				}
+			}
+		} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error: Output file not found in Encoder");
+		}
+		
 	}
 
 }
