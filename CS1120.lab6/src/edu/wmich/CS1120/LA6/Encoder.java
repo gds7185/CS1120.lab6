e1c2acb91b58a2be3a5f6876ccf4cf91af6355e9
Gavin <Gavin@10.80.140.104>
2018-11-20 11:18:56 -0500

    Gavin: wrote the part of the encoder class that reads the file into a string

--- CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
+++ CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
@@ -1,11 +1,30 @@
 package edu.wmich.CS1120.LA6;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Encoder implements IEncoder{
 
 	@Override
 	public void encode(String inputFileName, String outputFilePath) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
+		String fileRead = "";
+		int count = 0;
+		try {
+			File f = new File(inputFileName);
+			Scanner readFile = new Scanner(f);
+			while (readFile.hasNext()) {
+				fileRead = fileRead + readFile.nextLine();
+				count++;
+			}
+			readFile.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error: input file not found");
+		}
+		System.out.println("Content of input file: \n" + fileRead);
+		System.out.println(count);
 	}
 
 }
