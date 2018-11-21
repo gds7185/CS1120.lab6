6b350b1beabeb6ff694a80e1678f7727d687c005
jacob <jacob@DESKTOP-B30ALTI>
2018-11-21 14:45:38 -0500

    Jacob
    Just changing the format slightl

--- CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
+++ CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
@@ -12,30 +12,36 @@ public class Encoder implements IEncoder{
 
 	@Override
 	public void encode(String inputFileName, String outputFilePath) {
-		// TODO Auto-generated method stub	
+
 		String fileRead = "";
-		//int count = 0;
+		
 		try {
 			File f = new File(inputFileName);
 			Scanner readFile = new Scanner(f);
+			
 			while (readFile.hasNext()) {
+				
 				fileRead = fileRead + readFile.nextLine();
-				//count++;
 			}
+			
 			readFile.close();
 		} 
 		catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			System.out.println("Error: input file not found");
 		}
+		
 		System.out.println("Content of input file: \n" + fileRead);
 		//System.out.println(count);
 		Random rand = new Random();
 		int n = 0;
+		
 		try {
 			FileOutputStream toFile = new FileOutputStream(outputFilePath, true);
 			DataOutputStream output = new DataOutputStream(toFile);
+			
 			for(int i = 0; i < fileRead.length(); i ++) {
+				
 				n = rand.nextInt(20);	
 				String a = "";
 				a = a + fileRead.charAt(i);
@@ -50,19 +56,12 @@ public class Encoder implements IEncoder{
 			output.close();
 		} 
 		catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			
 			System.out.println("Error: Output file not found in Encoder");
 		}
 		catch (IOException e) {
+			
 			System.out.println("Error: problem with the encoder");
 		}
-		/*Scanner encrypted = new Scanner(outputFilePath);
-		String code = "";
-		while (encrypted.hasNext()) {
-			code = code + encrypted.nextLine();
-		}
-		System.out.println(code);*/
-		
 	}
-
 }
