c7960ecb077abb2150afcbd51f24afd1668b7747
Gavin <Gavin@10.80.140.104>
2018-11-21 00:09:52 -0500

    Gavin: finished writing the encoder class

--- CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
+++ CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -13,16 +14,17 @@ public class Encoder implements IEncoder{
 	public void encode(String inputFileName, String outputFilePath) {
 		// TODO Auto-generated method stub	
 		String fileRead = "";
-		int count = 0;
+		//int count = 0;
 		try {
 			File f = new File(inputFileName);
 			Scanner readFile = new Scanner(f);
 			while (readFile.hasNext()) {
 				fileRead = fileRead + readFile.nextLine();
-				count++;
+				//count++;
 			}
 			readFile.close();
-		} catch (FileNotFoundException e) {
+		} 
+		catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Error: input file not found");
 		}
@@ -31,22 +33,35 @@ public class Encoder implements IEncoder{
 		Random rand = new Random();
 		int n = 0;
 		try {
-			DataOutputStream output = new DataOutputStream(new FileOutputStream(outputFilePath));
+			FileOutputStream toFile = new FileOutputStream(outputFilePath, true);
+			DataOutputStream output = new DataOutputStream(toFile);
 			for(int i = 0; i < fileRead.length(); i ++) {
-				n = rand.nextInt(20);
-				String Out = "";
-				FileOutputStream toFile = new FileOutputStream(outputFilePath, true);
-				char a = fileRead.charAt(i);
-				Out = Out + a;
-				for (int j = 0; j<=n; j++) {
-					Out = Out + n;
-				}
+				n = rand.nextInt(20);	
+				String a = "";
+				a = a + fileRead.charAt(i);
+				int h = a.codePointAt(0);
+				output.writeChar(h);
+				//output.writeUTF(a);
+				output.writeInt(n);
+				output.writeByte(n);
 			}
+			output.writeInt(-1);
+			//System.out.println(output);
+			output.close();
 		} 
 		catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Error: Output file not found in Encoder");
 		}
+		catch (IOException e) {
+			System.out.println("Error: problem with the encoder");
+		}
+		/*Scanner encrypted = new Scanner(outputFilePath);
+		String code = "";
+		while (encrypted.hasNext()) {
+			code = code + encrypted.nextLine();
+		}
+		System.out.println(code);*/
 		
 	}
 
