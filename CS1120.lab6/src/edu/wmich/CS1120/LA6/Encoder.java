6214199850f912e23dfae8207325877d8a8aaafc
jacobcolegio-wmich <44441120+jacobcolegio-wmich@users.noreply.github.com>
2018-11-22 11:23:10 -0500

    Jacob
    Made some changes to how encoder works

--- CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
+++ CS1120.lab6/src/edu/wmich/CS1120/LA6/Encoder.java
@@ -1,68 +1,91 @@
 package edu.wmich.CS1120.LA6;
 
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Encoder implements IEncoder{
 
-	@Override
 	public void encode(String inputFileName, String outputFilePath) {
 
+		//Stores information from txt file
 		String fileRead = "";
 		
 		try {
+			
+			//Read file with scanner
 			File f = new File(inputFileName);
 			Scanner readFile = new Scanner(f);
 			
+			//Store txt file into fileRead string
 			while (readFile.hasNext()) {
 				
-				fileRead = fileRead + readFile.nextLine();
+				fileRead += readFile.nextLine();
 			}
-			
 			readFile.close();
 		} 
-		catch (FileNotFoundException e) {
+		catch (FileNotFoundException e) {		//Error handling
 			
 			System.out.println("Error: input file not found");
 		}
 		
-		System.out.println("Content of input file: \n" + fileRead);
-		//System.out.println(count);
-		Random rand = new Random();
+		System.out.println("Content of input file: \n\n" + fileRead);
+
+		Random rand = new Random();				//random object and variable
 		int n = 0;
-		System.out.println("Now encoding the file...");
+		
 		try {
-			FileOutputStream toFile = new FileOutputStream(outputFilePath, true);
-			DataOutputStream output = new DataOutputStream(toFile);
 			
-			for(int i = 0; i < fileRead.length(); i ++) {
+			//Using a RandomAccessFile to write our bytes to a binary file
+			RandomAccessFile randomAccess = new RandomAccessFile(outputFilePath, "rw");
+			randomAccess.seek(0);
+			
+			System.out.println("\nEncoding message...");
+			
+			//Iterate once for each character in message
+			for(int i = 0; i < fileRead.length(); i ++) {	
 				
+				//Set n to Random value from 1 to 20
 				n = rand.nextInt(20) + 1;	
-				String a = "";
-				a = a + fileRead.charAt(i);
-				int h = a.codePointAt(0);
-				output.writeChar(h);
-				//output.writeUTF(a);
-				output.writeInt(n);
-				output.writeByte(n);
+				
+				//Create byte array the size of n
+				byte[] bytes = new byte[n];
+				
+				//Populate bytes array with random values
+				rand.nextBytes(bytes);
+				
+				randomAccess.writeChar(fileRead.charAt(i));	//Write character
+				
+				//If we're on the last character
+				if(i == fileRead.length() - 1) {
+					
+					randomAccess.writeInt(-1);	//Set final value to -1
+				}
+				else {
+					
+					randomAccess.writeInt(n);	//Write int (number of random bytes)
+					
+					//Iterate through random bytes and write each value
+					for(int j = 0; j < bytes.length; j++) {
+						
+						randomAccess.writeByte(bytes[j]);
+					}
+				}
 			}
-			output.writeInt(-1);
-			//System.out.println(output);
-			output.close();
+			randomAccess.close();
 		} 
 		catch (FileNotFoundException e) {
 			
 			System.out.println("Error: Output file not found in Encoder");
 		}
 		catch (IOException e) {
-			 
+			
 			System.out.println("Error: problem with the encoder");
 		}
-		System.out.println("Done.");
+		
+		System.out.println("\nDone encoding message.\n");
 	}
 }
